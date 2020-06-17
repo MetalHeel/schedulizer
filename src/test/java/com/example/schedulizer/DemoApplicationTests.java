672b864760f48768b4afe6472e7bf2eb1812afa4
MetalHeel <MetalHeel@aol.com>
2020-06-17 15:49:32 -0500

    First commit.

---
+++ src/test/java/com/example/schedulizer/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
